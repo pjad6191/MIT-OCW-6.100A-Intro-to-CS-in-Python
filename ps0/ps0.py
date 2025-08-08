aa3b9c88bbf045dc190b6aeac9f2e0b7f6ac3985
pjad6191 <129990252+pjad6191@users.noreply.github.com>
2025-08-08 10:05:41 -0400

    Create ps0.py

---
+++ ps0/ps0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+This assignment introduces us to the programming environment Spyder, 
+programming in Python, and the general problem set structure. It is a simple 
+program the raises a number to a power and takes a logarithm. 
+"""
+ 
+import numpy 
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+
+print("x**y = ", x**y)
+print("log(x) = ", int(numpy.log2(x)))
